0aa6dcf9cf54c3f0953feccba8058e5ee158b6ca
Saurav1375 <sauravgupta1375@gmail.com>
2025-03-14 12:49:48 +0530

    added JWT Authentication with Email Validation

---
+++ src/main/java/org/example/springsecurity6/SpringSecurity6Application.java
@@ -0,0 +1,33 @@
+package org.example.springsecurity6;
+
+import org.example.springsecurity6.model.Role;
+import org.example.springsecurity6.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class SpringSecurity6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity6Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
+
+}
